6a17663ee872617b9389778957d250a5230cd76a
Kirill <Kristupas@protonmail.com>
2019-04-17 02:41:14 +0300

    Added new template with form. Changes in index Controller.

--- src/main/java/com/kristupas/MovieApp/controllers/IndexController.java
+++ src/main/java/com/kristupas/MovieApp/controllers/IndexController.java
@@ -4,11 +4,13 @@ import com.kristupas.MovieApp.services.MovieService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
+@RequestMapping("/")
 public class IndexController {
     private MovieService movieService;
 
@@ -16,7 +18,7 @@ public class IndexController {
         this.movieService = movieService;
     }
 
-    @RequestMapping("/")
+    @GetMapping
     public String indexPage(Model model){
         log.debug("Getting index page");
         model.addAttribute("movies",movieService.getMovies());
--- src/main/java/com/kristupas/MovieApp/controllers/MovieController.java
+++ src/main/java/com/kristupas/MovieApp/controllers/MovieController.java
@@ -1,12 +1,13 @@
 package com.kristupas.MovieApp.controllers;
 
+import com.kristupas.MovieApp.commands.MovieCommand;
 import com.kristupas.MovieApp.services.MovieService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/movies")
 public class MovieController {
 
     private MovieService movieService;
@@ -15,10 +16,23 @@ public class MovieController {
         this.movieService = movieService;
     }
 
-    @RequestMapping("/movies/show/{id}")
+    @GetMapping("show/{id}")
     public String showById(@PathVariable String id, Model model){
         model.addAttribute("movie",movieService.findById(new Long(id)));
         return "movies/show";
+    }
+
+    @GetMapping("new")
+    public String newMovie(Model model){
+        model.addAttribute("movie", new MovieCommand());
+        return "movies/movieinput.html";
+    }
+
+    @PostMapping("movie")
+    public String save(@ModelAttribute MovieCommand movieCommand){
+        MovieCommand savedCommand = movieService.saveMovieCommand(movieCommand);
+
+        return "redirect:/movies/show/" + savedCommand.getId();
     }
 
 }
